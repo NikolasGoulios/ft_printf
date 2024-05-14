3838865a7e1fe90f8fee35c415f2bc5a88b515d8
Nikolaos  Goulios <ngoulios@c1r5p4.hive.fi>
2024-05-14 11:50:34 +0300

    Organized Library

---
+++ Includes/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 11:03:05 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/05/14 11:03:07 by ngoulios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <libft.h>
+
+int	ft_printf(const char *s, ...);
+
+#endif
