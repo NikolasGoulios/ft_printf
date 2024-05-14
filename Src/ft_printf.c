dfe94ccc4bc4ac5fa5fe337c2973520bea9eddd3
Nikolaos  Goulios <ngoulios@c1r5p4.hive.fi>
2024-05-14 12:51:39 +0300

    prototype

--- Src/ft_printf.c
+++ Src/ft_printf.c
@@ -10,4 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	ft_printf(const char *, ...)
+{
+	int			count;
+	const char	s;
+	const char	c;
+
+	count = 0;
+	c = "%";
+	while (!s && s != c)
+	{
+		write(1, s, 1);
+		s++;
+	}
+	if (s == c)
+	{
+		++s;
+		if (s[s] == "c")
+		{
+			ft_putchar(va_arg);
+		}
+		else if (s[s] == "s")
+		{
+			ft_putstr(va_arg);
+		}
+		if (s[s] == "p")
+		{
+
+		}
+		if (s[s] == "i")
+		{
+			ft_putnbr(va_arg);
+		}
+		if (s[s] == "u")
+		{
+
+		}
+		if (s[s] == "x")
+		{
+
+		}
+		if (s[s] == "X")
+		{
+
+		}
+		if (s[s] == c)
+		{
+			ft_putchar("%");
+		}
+		
+
+	}
+
+}
+
 
