50704719065f934964db2bfb3fe37bef53c4fb02
Goullios <ngoulios97@gmail.com>
2024-05-16 22:28:46 +0300

    new functions

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	int	digit;
+
+	digit = 0;
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else
+	{
+		if (n > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			digit = n + 48;
+			write(fd, &digit, 1);
+		}
+	}
+}
