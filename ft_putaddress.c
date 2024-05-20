4687143e686fa1b00f41060889f5ae0deed8fb1b
Nikolaos  Goulios <ngoulios@c1r6p2.hive.fi>
2024-05-20 11:29:34 +0300

    new function

---
+++ ft_putaddress.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putaddress.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 09:58:55 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/05/20 10:22:57 by ngoulios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static unsigned long	ft_strlen(const char *s);
+static int				rec_print(unsigned long n, const char *base);
+
+int	ft_putaddress(void *format)
+{
+	unsigned long	n;
+	const char		*base;
+	int				len;
+
+	n = (unsigned long)format;
+	base = "0123456789abcdef";
+	len = ft_putstr("0x");
+	len += rec_print(n, base);
+	return (len);
+}
+
+static unsigned long	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+static int	rec_print(unsigned long n, const char *base)
+{
+	int	len;
+
+	len = 0;
+	if (n > (ft_strlen(base) - 1))
+		len += rec_print(n / ft_strlen(base), base);
+	len += ft_putchar(*(base + (n % ft_strlen(base))));
+	return (len);
+}
